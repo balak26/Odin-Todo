840c1fd066cfb6b8d486db1fbcaa873e54c42053
balak26 <bamlakgezahegn26@gmail.com>
2024-09-22 10:44:42 +0300

    webpakc config and boiler-plate code

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const { watch } = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+};
